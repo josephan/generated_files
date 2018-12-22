c9fefe748a9c717c645f4d120ca24b341ade9e2b
Joseph An <an.joseph891@gmail.com>
2018-12-22 05:15:50 -0500

    mix phx.gen.json Blog Post posts title body

---
+++ lib/generated_files_web/controllers/fallback_controller.ex
@@ -0,0 +1,22 @@
+defmodule GeneratedFilesWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use GeneratedFilesWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(GeneratedFilesWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(GeneratedFilesWeb.ErrorView)
+    |> render(:"404")
+  end
+end
